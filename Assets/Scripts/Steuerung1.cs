7b6bcd42f8968b48119c6a34b33dd78e78d0d3ef
Muffl <thorsten@thts.de>
2017-07-02 20:56:24 +0200

    Kollisionen merken

---
+++ Assets/Scripts/Steuerung1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steuerung1 : MonoBehaviour {
+
+
+    public float speed = 5;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    void FixedUpdate () {
+        rb.AddForce(new Vector2(Input.GetAxis("Horizontal")*speed, Input.GetAxis("Vertical")*speed), ForceMode2D.Force);
+	}
+}
